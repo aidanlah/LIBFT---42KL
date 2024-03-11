a5f7d9f92f86fa6e4da445ffab7712160bfc3744
Aidan Leong <aileong@u91z04s02.42kl.edu.my>
2024-03-11 22:53:03 +0800

    war machine done

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -14,18 +14,14 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	size_t			i;
-	unsigned char	*c1;
-	unsigned char	*c2;
+	size_t	i;
 
 	i = 0;
-	c1 = (unsigned char *)s1;
-	c2 = (unsigned char *)s2;
-	if (n == 0)
-		return (0);
-	while (i < (n - 1) && *(c1 + i) && *(c1 + i) == *(c2 + i))
+	while (i < n)
 	{
+		if (((unsigned char *)s1)[i] != ((unsigned char *)s2)[i])
+			return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
 		i++;
 	}
-	return (*(c1 + i) - *(c2 + i));
+	return (0);
 }
